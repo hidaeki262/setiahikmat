3a83666e5733b9be67980dcd0de94b46a60e06ca
“Kelvin <kelvintan@hitachi-ebworx.com>
2022-12-01 16:38:31 +0800

    update contact page

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
 const construction_worker = require("./construction_worker.jpeg");
+const contactLogo = require("./SETIA_HIKMAT_LOGO.png");
 
-export { construction_worker };
+export { construction_worker, contactLogo };
