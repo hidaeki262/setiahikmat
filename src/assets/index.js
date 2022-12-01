9245ccdd29d6d1aed502e6f4e45e24885891f1fe
“Kelvin <kelvintan@hitachi-ebworx.com>
2022-12-01 19:04:23 +0800

    update favicon, header logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
 const construction_worker = require("./construction_worker.jpeg");
 const companyNameLogo = require("./company_name_logo.png");
+const companyIcon = require("./company_icon.png");
 
-export { construction_worker, companyNameLogo };
+export { construction_worker, companyNameLogo, companyIcon };
