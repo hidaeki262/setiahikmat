0788905920820816cb9946786e7d25f957a3d47f
Kelvin Tan Kah Wai <kelvintankahwai@gmail.com>
2023-03-17 20:25:47 +0800

    add new images

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,5 +5,35 @@ const homeBg = require("./homebg.png");
 const imageList_01 = require("./imageList_01.jpg");
 const imageList_02 = require("./imageList_02.jpg");
 const imageList_03 = require("./imageList_03.jpg");
+const construction_01 = require("./construction_01.jpg");
+const construction_02 = require("./construction_02.jpg");
+const construction_03 = require("./construction_03.jpg");
+const construction_04 = require("./construction_04.jpg");
+const renovation_01 = require("./renovation_01.jpg");
+const renovation_02 = require("./renovation_02.jpg");
+const renovation_03 = require("./renovation_03.jpg");
+const customization_01 = require("./customization_01.jpg");
+const customization_02 = require("./customization_02.jpg");
+const customization_03 = require("./customization_03.png");
+const customization_04 = require("./customization_04.jpg");
 
-export { construction_worker, companyNameLogo, companyIcon, homeBg, imageList_01, imageList_02, imageList_03 };
+export {
+  construction_worker,
+  companyNameLogo,
+  companyIcon,
+  homeBg,
+  imageList_01,
+  imageList_02,
+  imageList_03,
+  construction_01,
+  construction_02,
+  construction_03,
+  construction_04,
+  renovation_01,
+  renovation_02,
+  renovation_03,
+  customization_01,
+  customization_02,
+  customization_03,
+  customization_04,
+};
