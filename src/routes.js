ff201e2650877e49f6e8be53cd2fb6b4bec5c0e1
MercuryOng <mercuryong@gmail.com>
2023-03-23 21:48:16 +0800

    Removed * path

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./views/MainPage";
 import ConstructionPage from "./views/Services/ConstructionPage";
 import CustomisePage from "./views/Services/CustomisePage";
@@ -18,7 +13,6 @@ function RouteRules() {
         <Route path="/renovation" element={<RenovationPage />} />
         <Route path="/customise" element={<CustomisePage />} />
         <Route path="/" element={<MainPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
   );
